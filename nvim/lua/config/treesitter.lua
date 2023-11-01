157b08df403e9e6655aae0382a0e7af7957f0de8
ketsumashu <k2mashu@icloud.com>
2023-11-01 15:07:49 +0900

    packer.nvim -> lazy.nvim

--- nvim/lua/config/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "lua", "rust" },
-    highlight = {
-        enable = true,
-    },
-    indent = {
-        enable = true,
-    },
-}
